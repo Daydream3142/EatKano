c10710550ebf1e5d0071467de6a3ab27fe081c97
Daydream3142 <94439373+Daydream3142@users.noreply.github.com>
2022-07-17 20:51:08 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -226,7 +226,7 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
         if (isNaN(cps) || cps === Infinity || _gameStartTime < 2) {
             cps = 0;
         }
-        return 15.56;
+        return cps;
     }
 
     function timer() {
@@ -462,8 +462,8 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
     function showGameScoreLayer(cps) {
         let l = $('#GameScoreLayer');
         let c = $(`#${_gameBBList[_gameBBListIndex - 1].id}`).attr('class').match(_ttreg)[1];
-        let score = 249;
-        let best = 249;
+        let score = (mode === MODE_ENDLESS ? cps : _gameScore);
+        let best = getBestScore(score);
         l.attr('class', l.attr('class').replace(/bgc\d/, 'bgc' + c));
         $('#GameScoreLayer-text').html(shareText(cps));
         let normalCond = legalDeviationTime() || mode !== MODE_NORMAL;
